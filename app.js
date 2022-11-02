590f1a94acc6c63d2e33ff8a1db5fb1d51814556
Achyutha Reddy <s550301@nwmissouri.edu>
2022-11-01 22:24:26 -0500

    added  gridbuild

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var animalRouter = require('./routes/animal');
+var gridbuildRouter = require('./routes/gridbuild');
+const { use } = require('./routes/animal');
 
 var app = express();
 
@@ -23,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/animal', animalRouter);
-
+app.use('/gridbuild', gridbuildRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
